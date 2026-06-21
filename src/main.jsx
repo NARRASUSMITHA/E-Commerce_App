bfc73db1afdedf3ab39335a66e396400f34629fe
NARRASUSMITHA <sushunarra@gmail.com>
2026-06-21 19:39:22 +0530

    Fix Tailwind dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link  } from "react-router-dom";
 import ShopContextProvider from './context/ShopContext.jsx';
+
+
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='/'>
-  <ShopContextProvider>
-    <App />
+    <ShopContextProvider>
+      <App />
     </ShopContextProvider>
-  </BrowserRouter>,
+  </BrowserRouter>
 )
